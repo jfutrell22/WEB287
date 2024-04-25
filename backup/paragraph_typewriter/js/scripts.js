3165f328b1849153deb3e065d868deeef68c445a
John <jrfutrell@my.waketech.edu>
2024-04-25 18:54:31 -0400

    JavaScript update

---
+++ backup/paragraph_typewriter/js/scripts.js
@@ -0,0 +1,45 @@
+/*!
+* Start Bootstrap - Bare v5.0.9 (https://startbootstrap.com/template/bare)
+* Copyright 2013-2023 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-bare/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// set up text to print, each item in array is new line
+var aText = new Array(
+    "Hello and welcome to my portfolio page. My name is John Futrell and I am an aspiring web developer. I have spent the last 10 years in the environmental field but wanted to try something new. Thats when I decided to start taking classes at Wake Tech and found my love for web development. I feel like I've learned so much these last couple years. From the basics of HTML/CSS and JavaScript to Bootstrap, PHP, Python, C#, and ASP.NET Core MVC I've thouroughly enjoyed learning about all of them in the classes I've taken at Wake Tech. I'm excited to continue learning and seeing what the future holds!"
+    );
+    var iSpeed = 30; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = aText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function typewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("typedtext");
+     
+     while ( iRow < iIndex ) {
+      sContents += aText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != aText.length ) {
+       iArrLength = aText[iIndex].length;
+       setTimeout("typewriter()", 500);
+      }
+     } else {
+      setTimeout("typewriter()", iSpeed);
+     }
+    }
+    
+    
+    typewriter();
--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,42 @@
 */
 // This file is intentionally blank
 // Use this file to add JavaScript to your project
+
+// set up text to print, each item in array is new line
+var hText = new Array(
+    "Welcome To My Web Portfolio!"
+    );
+    var iSpeed = 100; // time delay of print out
+    var iIndex = 0; // start printing array at this posision
+    var iArrLength = hText[0].length; // the length of the text array
+    var iScrollAt = 20; // start scrolling up at this many lines
+     
+    var iTextPos = 0; // initialise text position
+    var sContents = ''; // initialise contents variable
+    var iRow; // initialise current row
+     
+    function htypewriter()
+    {
+     sContents =  ' ';
+     iRow = Math.max(0, iIndex-iScrollAt);
+     var destination = document.getElementById("hometext");
+     
+     while ( iRow < iIndex ) {
+      sContents += hText[iRow++] + '<br />';
+     }
+     destination.innerHTML = sContents + hText[iIndex].substring(0, iTextPos) + "_";
+     if ( iTextPos++ == iArrLength ) {
+      iTextPos = 0;
+      iIndex++;
+      if ( iIndex != hText.length ) {
+       iArrLength = hText[iIndex].length;
+       setTimeout("htypewriter()", 500);
+      }
+     } else {
+      setTimeout("htypewriter()", iSpeed);
+     }
+    }
+    
+
+    
+    htypewriter();
